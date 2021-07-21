b1b0c56a2a96f4ad3c4637d74ce98c5e7f201563
tom <thomas.mcnutt.jr@gmail.com>
2021-07-21 19:43:04 -0400

    Updates

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -15,5 +15,23 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    @IBAction func calculateTip(_ sender: UISegmentedControl) {
+        // Get bill amount from text field input
+        let bill: Double = Double(billAmountTextField.text!) ?? 0.0
+        
+        // Get total tip by multiplying tip * tip %
+        let tipPercentages: [Double] = [0.15, 0.20, 0.25]
+        let tip: Double = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total: Double = bill + tip
+        //update tip and total amount labels
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
