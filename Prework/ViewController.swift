0fd9fdeff58c6c70699b412207819b752d3bafdc
tom <thomas.mcnutt.jr@gmail.com>
2021-07-27 23:32:02 -0400

    Added first responder and update README.md

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -12,14 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billAmountTextField.becomeFirstResponder()
+        
     }
-
-
+    let numberFormatter: NumberFormatter = NumberFormatter()
+    let localizesFormat: Bool = true
+    
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
-    
     
     @IBAction func calculateTip(_ sender: UISegmentedControl) {
         // Get bill amount from text field input
@@ -32,11 +34,8 @@ class ViewController: UIViewController {
         //update tip and total amount labels
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
-        twoSum()
     }
     
-    func twoSum() -> Int{
-        return 0
-    }
+
 }
 
