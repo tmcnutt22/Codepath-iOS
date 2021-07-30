4176b3b8e36615924ac4f55a5c0d0d5aac150753
tom <thomas.mcnutt.jr@gmail.com>
2021-07-29 23:25:39 -0400

    Finished app

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Prework
+//  TIPSY - A tipping calculator built in Swift and UIKit
 //
 //  Created by thomas mcnutt on 7/21/21.
 //
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //make the textfield the first responder and keep the decimal pad open
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
         
         //reload last bill amount
         billAmountTextField.text = String(format: "%.2f",
-            UserDefaults.standard.double(forKey: "savedAmount"))
+                                          UserDefaults.standard.double(forKey: "lastAmountEntered"))
+        tipAmountLabel.text = "0.00"
+        totalLabel.text = "0.00"
     }
     
     @IBOutlet weak var billAmountTextField: UITextField!
@@ -26,19 +28,26 @@ class ViewController: UIViewController {
     
     @IBAction func calculateTip(_ sender: UISegmentedControl) {
         // Get bill amount from text field input
-
         let bill: Double = Double(billAmountTextField.text!) ?? 0
-        //UserDefaults.standard.setValue(bill, forKey: "savedAmount")
-        UserDefaults.standard.setValue(bill, forKey: "savedAmount")
+        
+        //saves the last amount entered as a user default
+        UserDefaults.standard.setValue(bill, forKey: "lastAmountEntered")
+        
         // Get total tip by multiplying tip * tip %
         let tipPercentages: [Double] = [0.15, 0.20, 0.25]
         let tip: Double = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total: Double = bill + tip
+        
         //update tip and total amount labels
-        tipAmountLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.maximumFractionDigits = 2
+        numberFormatter.locale = Locale.current
+       
+        tipAmountLabel.text = numberFormatter.string(from: NSNumber(value: tip)) ?? "0"
+        totalLabel.text = numberFormatter.string(from: NSNumber(value: total)) ?? "0"
     }
     
-
+    
 }
 
--- README.md
+++ README.md
@@ -17,13 +17,14 @@ The following **optional** features are implemented:
 
 -   [ ] UI animations
 -   [x] Remembering the bill amount across app restarts (if <10mins)
--   [ ] Using locale-specific currency and currency thousands separators.
+-   [x] Using locale-specific currency and currency thousands separators.
 -   [x] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
 
 The following **additional** features are implemented:
 
--   [ ] List anything else that you can get done to improve the app functionality!
+-   List anything else that you can get done to improve the app functionality!
 -   [x] Dark / Light Mode UI
+-   [x] Persisted last bill amount using user defaults
 
 ## Video Walkthrough
 
